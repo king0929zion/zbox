95cb09cc1d712d1fc2d6c0a47c9b005852e006b6
Zion0929mu <zion0929mu@example.com>
2025-11-14 21:22:11 +0800

    fix: add material theme dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.4")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.4")
     implementation("androidx.datastore:datastore-preferences:1.1.1")
+    implementation("com.google.android.material:material:1.12.0")
 
     implementation(composeBom)
     androidTestImplementation(composeBom)
